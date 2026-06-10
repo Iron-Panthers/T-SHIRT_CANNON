abe3981ac48629a813e00a4c62303ee82ac6f696
Robbyrossross <preston.w.wei@gmail.com>
2026-06-09 18:51:28 -0700

    I change

--- src/main/java/frc/robot/subsystems/intake/intake_rack/IntakeRackIO.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.intake.intake_rack;
-
-import frc.robot.lib.generic_subsystems.superstructure.GenericSuperstructureIO;
-
-public interface IntakeRackIO extends GenericSuperstructureIO {}
--- src/main/java/frc/robot/subsystems/intake/intake_rollers/IntakeRollersIO.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.intake.intake_rollers;
-
-import frc.robot.lib.generic_subsystems.rollers.GenericRollersIO;
-
-public interface IntakeRollersIO extends GenericRollersIO {}
